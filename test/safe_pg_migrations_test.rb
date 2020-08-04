a9b2140798cfca3b89396cbee853ccd46d07fe8d
Fábio Guerreiro <fabio.guerreiro@doctolib.com>
2020-08-04 11:38:50 +0200

    add test to add_foreign_key idempotency

--- test/safe_pg_migrations_test.rb
+++ test/safe_pg_migrations_test.rb
@@ -365,6 +365,38 @@ class SafePgMigrationsTest < Minitest::Test
     ], calls
   end
 
+  def test_add_foreign_key_idem_potent
+    @connection.create_table(:users) { |t| t.string :email }
+    @connection.create_table(:messages) do |t|
+      t.string :message
+      t.bigint :user_id
+    end
+
+    @migration =
+      Class.new(ActiveRecord::Migration::Current) do
+        def change
+          2.times { add_foreign_key :messages, :users }
+        end
+      end.new
+
+    calls = record_calls(@connection, :execute) { run_migration }
+    binding.pry
+    assert_calls [
+      "SET statement_timeout TO '5s'",
+      'ALTER TABLE "messages" ADD CONSTRAINT "fk_rails_273a25a7a6" FOREIGN KEY ("user_id") REFERENCES "users" ("id") ' \
+        'NOT VALID',
+      "SET statement_timeout TO '70s'",
+      'SET statement_timeout TO 0',
+      'ALTER TABLE "messages" VALIDATE CONSTRAINT "fk_rails_273a25a7a6"',
+      "SET statement_timeout TO '70s'",
+      "SET statement_timeout TO '5s'",
+      "SET statement_timeout TO '70s'",
+      "SET statement_timeout TO 0",
+      'ALTER TABLE "messages" VALIDATE CONSTRAINT "fk_rails_273a25a7a6"',
+      "SET statement_timeout TO '70s'",
+    ], calls
+  end
+
   def test_add_foreign_key_with_validation
     @connection.create_table(:users) { |t| t.string :email }
     @connection.create_table(:messages) do |t|
