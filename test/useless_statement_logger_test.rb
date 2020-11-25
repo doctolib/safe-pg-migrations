2b329547685cf003c20e9e6aedbc7b1335e69c9e
Thomas Hareau <thomas.hareau@doctolib.com>
2020-11-25 12:59:15 +0100

    Move test to dedicated file

---
+++ test/useless_statement_logger_test.rb
@@ -0,0 +1,86 @@
+# frozen_string_literal: true
+
+require 'test_helper'
+
+class UselessStatementLoggerTest < MiniTest::Unit::TestCase
+  def setup
+    SafePgMigrations.instance_variable_set(:@config, nil)
+    @connection = ActiveRecord::Base.connection
+    @verbose_was = ActiveRecord::Migration.verbose
+    @connection.create_table(:schema_migrations) { |t| t.string :version }
+    ActiveRecord::SchemaMigration.create_table
+    ActiveRecord::Migration.verbose = false
+    @connection.execute("SET statement_timeout TO '70s'")
+    @connection.execute("SET lock_timeout TO '70s'")
+  end
+
+  def teardown
+    ActiveRecord::SchemaMigration.drop_table
+    @connection.execute('SET statement_timeout TO 0')
+    @connection.execute("SET lock_timeout TO '30s'")
+    @connection.drop_table(:users, if_exists: true)
+    ActiveRecord::Migration.verbose = @verbose_was
+  end
+
+  def test_ddl_transactions
+    @migration =
+      Class.new(ActiveRecord::Migration::Current) do
+        disable_ddl_transaction!
+
+        def change
+          create_table(:users) { |t| t.string :email }
+        end
+      end.new
+
+    write_calls = record_calls(SafePgMigrations, :say) { run_migration }.map(&:first)
+
+    assert_includes write_calls, '/!\ No need to explicitly disable DDL transaction, safe-pg-migrations does it for you'
+  end
+
+  def test_no_warning_when_no_ddl_transaction
+    @migration =
+      Class.new(ActiveRecord::Migration::Current) do
+        def change
+          create_table(:users) { |t| t.string :email }
+        end
+      end.new
+
+    write_calls = record_calls(SafePgMigrations, :say) { run_migration }.map(&:first)
+
+    refute_includes write_calls, '/!\ No need to explicitly disable DDL transaction, safe-pg-migrations does it for you'
+  end
+
+  def test_add_index_concurrently
+    @connection.create_table(:users) { |t| t.string :email }
+    @migration =
+      Class.new(ActiveRecord::Migration::Current) do
+        def change
+          add_index :users, :email, algorithm: :concurrently
+        end
+      end.new
+
+    write_calls = record_calls(SafePgMigrations, :say) { run_migration }.map(&:first)
+
+    assert_includes(
+      write_calls,
+      '/!\ No need to explicitly use `algorithm: :concurrently`, safe-pg-migrations does it for you'
+    )
+  end
+
+  def test_no_warning_when_no_index_concurrently
+    @connection.create_table(:users) { |t| t.string :email }
+    @migration =
+      Class.new(ActiveRecord::Migration::Current) do
+        def change
+          add_index :users, :email
+        end
+      end.new
+
+    write_calls = record_calls(SafePgMigrations, :say) { run_migration }.map(&:first)
+
+    refute_includes(
+      write_calls,
+      '/!\ No need to explicitly use `algorithm: :concurrently`, safe-pg-migrations does it for you'
+    )
+  end
+end
