41f83496a3195a8dfc05f98940d43c6694964581
Thomas Hareau <thomas.hareau@doctolib.com>
2020-11-27 10:36:44 +0100

    Fix

--- lib/safe-pg-migrations/plugins/useless_statements_logger.rb
+++ lib/safe-pg-migrations/plugins/useless_statements_logger.rb
@@ -11,22 +11,22 @@ module SafePgMigrations
       super
     end
 
-    def remove_index(*args, **options)
-      warn_for_index(**options)
+    def remove_index(table_name, options = {})
+      warn_for_index(options) if options.is_a? Hash
       super
     end
 
-    def add_foreign_key(*args, validate: nil)
-      if validate == false
+    def add_foreign_key(*args, **options)
+      if options[:validate] == false
         UselessStatementsLogger.warn_useless '`validate: :false`', 'https://github.com/doctolib/safe-pg-migrations#safe_add_foreign_key'
       end
       super
     end
 
-    def warn_for_index(algorithm: nil)
-      if algorithm == :concurrently
-        UselessStatementsLogger.warn_useless '`algorithm: :concurrently`', 'https://github.com/doctolib/safe-pg-migrations#safe_add_remove_index'
-      end
+    def warn_for_index(**options)
+      return unless options[:algorithm] == :concurrently
+
+      UselessStatementsLogger.warn_useless '`algorithm: :concurrently`', 'https://github.com/doctolib/safe-pg-migrations#safe_add_remove_index'
     end
 
     def self.warn_useless(action, link = nil, *args)
