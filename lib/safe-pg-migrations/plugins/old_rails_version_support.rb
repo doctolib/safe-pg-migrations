fcddf3147e2c9fcff8de28d18ef0b92b2beac2a9
Thomas Hareau <thomas.hareau@doctolib.com>
2022-01-13 16:54:36 +0100

    Fix old version of rails

---
+++ lib/safe-pg-migrations/plugins/old_rails_version_support.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+module SafePgMigrations
+  module OldRailsVersionSupport
+    ACTIVE_RECORD_VERSION = ActiveRecord::VERSION::STRING
+
+    ruby2_keywords def validate_foreign_key(from_table, to_table = nil, **options)
+      if ACTIVE_RECORD_VERSION < '6'
+        super(from_table, to_table || options)
+      else
+        super(from_table, to_table, **options)
+      end
+    end
+
+    ruby2_keywords def foreign_key_exists?(from_table, to_table = nil, **options)
+      if ACTIVE_RECORD_VERSION < '6'
+        super(from_table, to_table || options)
+      else
+        super(*args, **options)
+      end
+    end
+  end
+end
