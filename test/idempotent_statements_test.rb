2de31c1118fd597ad9fcf076ada896e2f8157ea4
Thomas Hareau <thomas.hareau@doctolib.com>
2022-01-14 18:51:02 +0100

    Remove create_table test

--- test/idempotent_statements_test.rb
+++ test/idempotent_statements_test.rb
@@ -328,4 +328,43 @@ class IdempotentStatementsTest < Minitest::Test
                  ], write_calls.map(&:first).values_at(0, 1, 3)
   end
 
+  def test_create_table
+    # Simulates an interruption between the table creation and the index creation
+    @connection.create_table(:users) do |t|
+      t.string :name, index: true
+      t.string :email
+    end
+
+    @migration =
+      Class.new(ActiveRecord::Migration::Current) do
+        def change
+          create_table(:users) do |t|
+            t.string :name, index: true
+            t.string :email, index: true
+          end
+        end
+      end.new
+
+    calls = record_calls(@connection, :execute) { run_migration }
+    indexes = ActiveRecord::Base.connection.indexes :users
+    refute_empty indexes
+    assert_equal 'index_users_on_email', indexes.first.name
+
+    refute_includes flat_calls(calls), 'CREATE INDEX CONCURRENTLY "index_users_on_name" ON "users" ("name")'
+
+    assert_calls [
+                   "SET statement_timeout TO '5s'",
+                   'SET statement_timeout TO 0',
+                   'SET lock_timeout TO 0',
+                   "SET lock_timeout TO '5s'",
+                   "SET statement_timeout TO '5s'",
+                   'SET statement_timeout TO 0',
+                   'SET lock_timeout TO 0',
+                   'CREATE INDEX "index_users_on_email" ON "users" ("email")',
+                   "SET lock_timeout TO '5s'",
+                   "SET statement_timeout TO '5s'",
+                   "SET statement_timeout TO '70s'",
+                 ], calls
+  end
+
 end
--- test/statement_insurer_test.rb
+++ test/statement_insurer_test.rb
@@ -264,4 +264,37 @@ class StatementInsurerTest < Minitest::Test
                    "SET statement_timeout TO '70s'",
                  ], calls
   end
+
+  def test_create_table
+    @migration =
+      Class.new(ActiveRecord::Migration::Current) do
+        def change
+          create_table(:users) do |t|
+            t.string :email
+            t.references :user, foreign_key: true
+          end
+        end
+      end.new
+
+    calls = record_calls(@connection, :execute) { run_migration }
+    assert_calls [
+                   "SET statement_timeout TO '5s'",
+
+                   # Create the table with constraints.
+                   'CREATE TABLE "users" ("id" bigserial primary key, "email" character varying, "user_id" bigint, ' \
+        'CONSTRAINT "fk_rails_6d0b8b3c2f" FOREIGN KEY ("user_id") REFERENCES "users" ("id") )',
+
+                   # Create the index.
+                   'SET statement_timeout TO 0',
+                   'SET lock_timeout TO 0',
+                   'CREATE INDEX "index_users_on_user_id" ON "users" ("user_id")',
+                   "SET lock_timeout TO '5s'",
+                   "SET statement_timeout TO '5s'",
+
+                   "SET statement_timeout TO '70s'",
+                 ], calls
+
+    run_migration(:down)
+    refute @connection.table_exists?(:users)
+  end
 end
