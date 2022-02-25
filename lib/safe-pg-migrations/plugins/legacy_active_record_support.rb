bf5e602a756992282116fdbf85e030ea7dfa9532
Sunny Ripert <sunny@sunfox.org>
2022-02-25 10:18:02 +0100

    Remove ruby2_keywords flag warning

--- lib/safe-pg-migrations/plugins/legacy_active_record_support.rb
+++ lib/safe-pg-migrations/plugins/legacy_active_record_support.rb
@@ -2,16 +2,16 @@
 
 module SafePgMigrations
   module LegacyActiveRecordSupport
-    ruby2_keywords def validate_foreign_key(from_table, to_table = nil, **options)
+    ruby2_keywords def validate_foreign_key(from_table, to_table = nil, *options)
       return super(from_table, to_table || options) unless satisfied? '>=6.0.0'
 
-      super(from_table, to_table, **options)
+      super(from_table, to_table, *options)
     end
 
-    ruby2_keywords def foreign_key_exists?(from_table, to_table = nil, **options)
+    ruby2_keywords def foreign_key_exists?(from_table, to_table = nil, *options)
       return super(from_table, to_table || options) unless satisfied? '>=6.0.0'
 
-      super(from_table, to_table, **options)
+      super(from_table, to_table, *options)
     end
 
     private
