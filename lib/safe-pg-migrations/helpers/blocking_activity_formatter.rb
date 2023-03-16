23c006467a45ba47b5ee3ee8585d2e0ca6d351de
Thomas Hareau <thomas.hareau@doctolib.com>
2023-03-16 16:28:24 +0100

    Rubocop

--- lib/safe-pg-migrations/helpers/blocking_activity_formatter.rb
+++ lib/safe-pg-migrations/helpers/blocking_activity_formatter.rb
@@ -5,20 +5,20 @@ module SafePgMigrations
     module BlockingActivityFormatter
       def log_queries(queries)
         if queries.empty?
-          SafePgMigrations.say "Could not find any blocking query.", true
+          SafePgMigrations.say 'Could not find any blocking query.', true
         else
           SafePgMigrations.say(
-            "Statement was being blocked by the following #{"query".pluralize(queries.size)}:",
+            "Statement was being blocked by the following #{'query'.pluralize(queries.size)}:",
             true
           )
-          SafePgMigrations.say "", true
+          SafePgMigrations.say '', true
           output_blocking_queries(queries)
           SafePgMigrations.say(
-            "Beware, some of those queries might run in a transaction. In this case the locking query might be " \
-              "located elsewhere in the transaction",
+            'Beware, some of those queries might run in a transaction. In this case the locking query might be ' \
+            'located elsewhere in the transaction',
             true
           )
-          SafePgMigrations.say "", true
+          SafePgMigrations.say '', true
         end
       end
 
@@ -38,10 +38,10 @@ module SafePgMigrations
       def output_confidentially_blocking_queries(queries)
         queries.each do |start_time, locktype, mode, pid, transactionid|
           SafePgMigrations.say(
-            "#{format_start_time(start_time)}: lock type: #{locktype || "null"}, " \
-              "lock mode: #{mode || "null"}, " \
-              "lock pid: #{pid || "null"}, " \
-              "lock transactionid: #{transactionid || "null"}",
+            "#{format_start_time(start_time)}: lock type: #{locktype || 'null'}, " \
+            "lock mode: #{mode || 'null'}, " \
+            "lock pid: #{pid || 'null'}, " \
+            "lock transactionid: #{transactionid || 'null'}",
             true
           )
         end
@@ -50,7 +50,7 @@ module SafePgMigrations
       def format_start_time(start_time, reference_time = Time.now)
         start_time = Time.parse(start_time) unless start_time.is_a? Time
         duration = (reference_time - start_time).round
-        "transaction started #{duration} #{"second".pluralize(duration)} ago"
+        "transaction started #{duration} #{'second'.pluralize(duration)} ago"
       end
     end
   end
--- lib/safe-pg-migrations/helpers/blocking_activity_selector.rb
+++ lib/safe-pg-migrations/helpers/blocking_activity_selector.rb
@@ -27,25 +27,25 @@ module SafePgMigrations
           )
 
         <<~SQL.squish
-        SELECT #{columns.join(", ")}
-        FROM pg_catalog.pg_locks           blocked_locks
-        JOIN pg_catalog.pg_stat_activity   blocked_activity
-          ON blocked_activity.pid = blocked_locks.pid
-        JOIN pg_catalog.pg_locks           blocking_locks
-          ON blocking_locks.locktype = blocked_locks.locktype
-          AND blocking_locks.DATABASE      IS NOT DISTINCT FROM blocked_locks.DATABASE
-          AND blocking_locks.relation      IS NOT DISTINCT FROM blocked_locks.relation
-          AND blocking_locks.page          IS NOT DISTINCT FROM blocked_locks.page
-          AND blocking_locks.tuple         IS NOT DISTINCT FROM blocked_locks.tuple
-          AND blocking_locks.virtualxid    IS NOT DISTINCT FROM blocked_locks.virtualxid
-          AND blocking_locks.transactionid IS NOT DISTINCT FROM blocked_locks.transactionid
-          AND blocking_locks.classid       IS NOT DISTINCT FROM blocked_locks.classid
-          AND blocking_locks.objid         IS NOT DISTINCT FROM blocked_locks.objid
-          AND blocking_locks.objsubid      IS NOT DISTINCT FROM blocked_locks.objsubid
-          AND blocking_locks.pid != blocked_locks.pid
-        JOIN pg_catalog.pg_stat_activity   blocking_activity
-          ON blocking_activity.pid = blocking_locks.pid
-        WHERE blocked_locks.pid = %d
+          SELECT #{columns.join(', ')}
+          FROM pg_catalog.pg_locks           blocked_locks
+          JOIN pg_catalog.pg_stat_activity   blocked_activity
+            ON blocked_activity.pid = blocked_locks.pid
+          JOIN pg_catalog.pg_locks           blocking_locks
+            ON blocking_locks.locktype = blocked_locks.locktype
+            AND blocking_locks.DATABASE      IS NOT DISTINCT FROM blocked_locks.DATABASE
+            AND blocking_locks.relation      IS NOT DISTINCT FROM blocked_locks.relation
+            AND blocking_locks.page          IS NOT DISTINCT FROM blocked_locks.page
+            AND blocking_locks.tuple         IS NOT DISTINCT FROM blocked_locks.tuple
+            AND blocking_locks.virtualxid    IS NOT DISTINCT FROM blocked_locks.virtualxid
+            AND blocking_locks.transactionid IS NOT DISTINCT FROM blocked_locks.transactionid
+            AND blocking_locks.classid       IS NOT DISTINCT FROM blocked_locks.classid
+            AND blocking_locks.objid         IS NOT DISTINCT FROM blocked_locks.objid
+            AND blocking_locks.objsubid      IS NOT DISTINCT FROM blocked_locks.objsubid
+            AND blocking_locks.pid != blocked_locks.pid
+          JOIN pg_catalog.pg_stat_activity   blocking_activity
+            ON blocking_activity.pid = blocking_locks.pid
+          WHERE blocked_locks.pid = %d
         SQL
       end
     end
